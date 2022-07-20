636adcf4106b137c435ae2d32eacac5ef62c937f
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-07-20 20:02:50 +0900

    Add basic structure for blog app

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get('posts/{post}', function ($slug) {
+
+
+    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
+        return redirect('/');
+    }
+
+    $post = cache()->remember("posts.{$slug}", 1200, fn () => file_get_contents($path));
+
+    return view('post', ['post' => $post]);
+})->where('post', '[A-z_\-]+');
