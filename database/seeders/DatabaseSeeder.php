a16436844275d849f485b54b319df333500c6d32
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-07-21 18:21:48 +0900

    Add seed data of User, Post and Category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +17,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // User::truncate();
+        // Post::truncate();
+        // Category::truncate();
+
+        $user = User::factory()->create([
+            'name' => 'John Doe'
+        ]);
+
+        Post::factory(5)->create([
+            'user_id' => $user->id
+        ]);
+
+        // $user = User::factory()->create();
+
+        // $personal =  Category::create([
+        //     'name' => 'Personal',
+        //     'slug' => 'personal'
+        // ]);
+
+        // $family = Category::create([
+        //     'name' => 'Family',
+        //     'slug' => 'family'
+        // ]);
+
+        // $work = Category::create([
+        //     'name' => 'Work',
+        //     'slug' => 'work'
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $family->id,
+        //     'title' => 'My family post',
+        //     'slug' => 'my-first-post',
+        //     'excerpt'  => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
+        //     'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio rerum quibusdam ab aliquam dicta quas ratione totam officiis! Quae a debitis cum adipisci voluptatum et numquam facilis magnam, quia placeat.</p>'
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $work->id,
+        //     'title' => 'My work post',
+        //     'slug' => 'my-work-post',
+        //     'excerpt'  => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
+        //     'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio rerum quibusdam ab aliquam dicta quas ratione totam officiis! Quae a debitis cum adipisci voluptatum et numquam facilis magnam, quia placeat.</p>'
+        // ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
