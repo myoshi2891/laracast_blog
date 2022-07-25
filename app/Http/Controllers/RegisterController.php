a03abc927cc260940c075d4725ecfb93b1450257
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-07-25 21:23:08 +0900

    Add registration form for User

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('register.create');
+    }
+
+    public function store()
+    {
+
+        $attributes = request()->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|max:255|min:3',
+            'email' => 'required|email|max:255',
+            'password' => ['required', 'max:255', 'min:7']
+        ]);
+
+        // $attributes['password'] = bcrypt($attributes['password']);
+
+        User::create($attributes);
+
+        // User::create([
+        //     'name' => $attributes['name'],
+        //     'password' => bcrypt($attributes['password'])
+        // ]);
+
+        return redirect('/');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,12 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'password',
+    // ];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -41,6 +42,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 
     public function posts()
     {
