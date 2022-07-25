20cfa4f5953b8112644aa0bdb53b26456f2953c8
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-07-26 02:33:58 +0900

    Add validation for the register from

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -16,10 +17,11 @@ class RegisterController extends Controller
     {
 
         $attributes = request()->validate([
-            'name' => 'required|max:255',
-            'username' => 'required|max:255|min:3',
-            'email' => 'required|email|max:255',
-            'password' => ['required', 'max:255', 'min:7']
+            'name' => 'required|max:55',
+            'username' => ['required', 'max:55', 'min:3', Rule::unique('users', 'username')],
+            // 'username' => 'required|max:55|min:3|unique:users,username',
+            'email' => 'required|email|max:55|unique:users,email',
+            'password' => ['required', 'max:55', 'min:7']
         ]);
 
         // $attributes['password'] = bcrypt($attributes['password']);
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -10,26 +10,33 @@
                         Name
                     </label>
 
-                    <input type="text" class="border border-grey-400 p-2 w-full" name="name" id="name"
-                        required>
+                    <input type="text" class="border border-grey-400 p-2 w-full" value="{{ old('name') }}"
+                        name="name" id="name" required>
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
                     <label for="username" class="block mb-2 uppercase font-bold text-xs text-gray-700">
                         Username
                     </label>
-
-                    <input type="text" class="border border-grey-400 p-2 w-full" name="username" id="username"
-                        required>
+                    <input type="text" class="border border-grey-400 p-2 w-full" value="{{ old('username') }}"
+                        name="username" id="username" required>
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
                     <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">
                         Email
                     </label>
-
-                    <input type="email" class="border border-grey-400 p-2 w-full" name="email" id="email"
-                        required>
+                    <input type="email" class="border border-grey-400 p-2 w-full" value="{{ old('email') }}"
+                        name="email" id="email" required>
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
 
@@ -37,9 +44,11 @@
                     <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">
                         Password
                     </label>
-
                     <input type="password" class="border border-grey-400 p-2 w-full" name="password" id="password"
                         required>
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -47,6 +56,14 @@
                         Submit
                     </button>
                 </div>
+
+                {{-- @if ($errors->any())
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li class="text-red-500 text-xs">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif --}}
             </form>
         </main>
     </section>
